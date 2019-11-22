e76011dc0942e582a9093d4471a237bc937b124a
Noam Rosenthal <noam.j.rosenthal@gmail.com>
2019-11-22 12:26:01 +0200

    Move formulas to external file

--- src/Simple.tsx
+++ src/Simple.tsx
@@ -1,6 +1,6 @@
-import  React, {useMemo, useCallback, FormEvent, useEffect} from 'react'
+import  React, {useState, useMemo, FormEvent} from 'react'
 import { Switch, Route, Redirect }from 'react-router'
-import { useQueryParam, NumberParam, StringParam } from 'use-query-params'
+import calcTax from './formulas'
 
 const Label = ({children, id}: {children: string, id: string}) => <label htmlFor={id} className="col-sm-2 col-form-label">{children}</label>
 const YesNo = ({id, label, disabled, defaultValue, onChange}: 
@@ -12,7 +12,6 @@ const YesNo = ({id, label, disabled, defaultValue, onChange}:
 </Row>
 
 const defaultIncome = 7550
-const defaultSex = 'f'
 const Row = ({children, label, id}: {children: JSX.Element, label: string, id: string}) => 
     <div key={id} className="form-group row">
         <Label id={id}>{label}</Label>
@@ -30,66 +29,36 @@ const Output = ({label, value}: {label: string, value: string | number}) =>
 const shekel = (n: number) => `${Number(Math.floor(n)).toLocaleString()} ₪`
 
 const Simple = () => {
-    const [hasPartner, setHasPartner] = useQueryParam('hasPartner', StringParam)
-    useEffect(() => {if (typeof hasPartner === 'undefined') setHasPartner('false') }, [hasPartner, setHasPartner])
-    const [income, setIncome] = useQueryParam('income', NumberParam)
-    useEffect(() => {if (typeof income === 'undefined') setIncome(defaultIncome) }, [income, setIncome])
-    const [numChildren, setNumChildren] = useQueryParam('children', NumberParam)
-    useEffect(() => {if (typeof numChildren === 'undefined') setNumChildren(0) }, [numChildren, setNumChildren])
-    const [partnerIncome, setPartnerIncome] = useQueryParam('partnerIncome', NumberParam)
-    useEffect(() => {if (typeof partnerIncome === 'undefined') setPartnerIncome(defaultIncome) }, [partnerIncome, setPartnerIncome])
-    const [sex, setSex] = useQueryParam('sex', StringParam)
-    useEffect(() => {if (typeof sex === 'undefined') setSex(defaultSex) }, [sex, setSex])
+    const [hasPartner, setHasPartner] = useState<boolean>(false)
+    const [income, setIncome] = useState<number>(defaultIncome)
+    const [numChildren, setNumChildren] = useState<number>(0)
+    const [partnerIncome, setPartnerIncome] = useState<number>(defaultIncome)
+    const [sex, setSex] = useState<'m' | 'f'>('f')
+    const [showResults, setShowResults] = useState<boolean>(false)
 
-    const doesHavePartner = useMemo(() => hasPartner === 'true', [hasPartner])
-
-    const bonusPoints = useMemo(() => 2.25 + (sex === 'f' ? 0.5 : 0) + 
-        ((sex === 'f' || !doesHavePartner) ? (numChildren || 0) : 0), [sex, !doesHavePartner, numChildren])
-
-    const perBonusPoint = 2580
-    const taxReduction = useMemo(() => bonusPoints * perBonusPoint, [bonusPoints])
-    const yearlyIncome = useMemo(() => (income || 0) * 12, [income])
-
-    const taxStep = useCallback((v: number) => {
-        const steps = [[0, 0], [74640, .1], [107040, .14], [171840, .2], [238800, .31], [496920, .35], [640000, .47], [Infinity, .5]]
-        let total = 0
-        for (let i = 0; i < steps.length; ++i) {
-            const [value, rate] = steps[i]
-            if (v <= value) {
-                return total + (v - value) * rate
-            } else if (i) {
-                total += (value - steps[i - 1][0]) * rate
-            }
-        }
-
-        return Math.max(0, total)
-    }, [])
-    const taxWithoutReductions = useMemo(() => taxStep(yearlyIncome), [yearlyIncome, taxStep])
-    const yearlyPartnerIncome = useMemo(() => (doesHavePartner && partnerIncome) ? partnerIncome * 12 : 0, [doesHavePartner, partnerIncome])
-    const incomeTax = useMemo(() => Math.max(0, taxWithoutReductions - taxReduction), [taxWithoutReductions, taxReduction])
-    const partnerTax = useMemo(() => Math.max(0, doesHavePartner ? taxStep(yearlyPartnerIncome || 0) - 2.25 * perBonusPoint : 0), [yearlyPartnerIncome, taxStep, doesHavePartner])
-    const netAnnualPartnerIncome = useMemo(() => yearlyPartnerIncome ? yearlyPartnerIncome - partnerTax : 0, [yearlyPartnerIncome, partnerTax])
-    const netIncome = useMemo(() => yearlyIncome - incomeTax, [yearlyIncome, incomeTax])
-    const householdIncome = useMemo(() => netIncome + netAnnualPartnerIncome, [netIncome, netAnnualPartnerIncome])
-    const netMonthlyHouseholdIncome = useMemo(() => householdIncome / 12, [householdIncome])
-    const decile = useMemo(() => {
-        const deciles = [0, 4214, 6558, 8580, 11028, 13323, 16037, 19196, 23274, 29938, Infinity]
-        for (let i = 0; i < deciles.length; ++i) {
-            if (netMonthlyHouseholdIncome <= deciles[i]) {
-                return i
-            }
-        }
-    }, [netMonthlyHouseholdIncome])
-
-    const vatPerDecile = [0, 1190, 1208, 1355, 1606, 1731, 1885, 2014, 2189, 2389, 2920]
-
-    const monthlyVat = useMemo(() => vatPerDecile[decile || 0], [decile, vatPerDecile])
-    const householdAnnualVat = useMemo(() => monthlyVat * 12, [monthlyVat])
-    const myAnnualVat = useMemo(() => householdAnnualVat / (doesHavePartner ? 2 : 1), [doesHavePartner, householdAnnualVat])
-    const totalAnnualTax = useMemo(() => myAnnualVat + Math.max(0, incomeTax), [myAnnualVat, incomeTax])
-
+    const {
+        totalAnnualTax,
+        annualIncome,
+        netIncome,
+        householdIncome,
+        netMonthlyHouseholdIncome,
+        decile,
+        monthlyVat,
+        myAnnualVat,
+        taxWithoutReductions,
+        householdAnnualVat,
+        netAnnualPartnerIncome,
+        partnerTax,
+        incomeTax,
+        annualPartnerIncome,
+        bonusPoints,
+        taxReduction
+    } = useMemo(() => calcTax({hasPartner, sex, numChildren, partnerIncome, income}), [
+        hasPartner, sex, numChildren, partnerIncome, income
+    ])
     const submit = (e: FormEvent) => {
         e.preventDefault()
+        setShowResults(true)
     }
 
     return <Switch>
@@ -99,9 +68,10 @@ const Simple = () => {
                 <a href="/simple/start">יאללה בואו נתקדם</a>
             </Route>
             <Route path="/simple/start">
-                <form method="get" action="/simple/results">
+            { showResults ? <Redirect to="/simple/results" /> : 
+            <form onSubmit={submit}>
                     <Row label="מין" id="sex">
-                        <select onChange={({target}) => setSex(target.value)} value={sex}>
+                        <select onChange={({target}) => setSex(target.value as 'm' | 'f')} value={sex}>
                         <option value="f">נקבה</option>
                         <option value="m">זכר</option>
                         </select>
@@ -120,22 +90,22 @@ const Simple = () => {
                         </select>
                     </Row>
                     <YesNo key="car" label='האם יש ברשותך רכב?' id="car" />
-                    <YesNo key="partner" label='האם יש לך בן/בת זוג?' id="partner" defaultValue={doesHavePartner} onChange={v => setHasPartner(String(v))} />
+                    <YesNo key="partner" label='האם יש לך בן/בת זוג?' id="partner" defaultValue={hasPartner} onChange={setHasPartner} />
                     <Row label="הכנסה חודשית ממוצעת של בן/בת הזוג (כולל קצבאות) ברוטו" id="partnerIncome">
                         <input type="number" defaultValue={partnerIncome} disabled={!hasPartner} onChange={v => setPartnerIncome(+v)} />
                     </Row>
                     <div className="form-group row">
                     <div className="col-sm-4">
-                      <button type="submit" className="btn btn-primary" onSubmit={submit}>שלח</button>
+                      <button type="submit" className="btn btn-primary">שלח</button>
                     </div>
                   </div>
-                  </form>
+                  </form>}
             </Route>
             <Route path="/simple/results">
                 <h3>עיבוד נתונים</h3>
-                <Output label="הכנסה שנתית" value={shekel(yearlyIncome)} />
+                <Output label="הכנסה שנתית" value={shekel(annualIncome)} />
                 <Output label="הכנסה נטו שלי (שנתי)" value={shekel(netIncome)} />
-                <Output label="הכנסה שנתית של בן/בת הזוג" value={shekel(netAnnualPartnerIncome)} />
+                <Output label="הכנסה שנתית של בן/בת הזוג" value={shekel(annualPartnerIncome)} />
                 <Output label="מס הכנסה ללא נקודות זיכוי" value={shekel(taxWithoutReductions)} />
                 <Output label="מספר נקודות זיכוי" value={bonusPoints} />
                 <Output label="הפחת מס בזכות נקודות זיכוי" value={shekel(taxReduction)} />
---
+++ src/formulas.ts
@@ -0,0 +1,74 @@
+export type TaxInput = {
+    hasPartner: boolean
+    sex: 'm' | 'f'
+    numChildren: number
+    income: number
+    partnerIncome: number
+}
+export default function caculateTax({hasPartner, sex, numChildren, partnerIncome, income} : TaxInput) {
+    const partnerIncomeForCalculation = hasPartner ? partnerIncome : 0 
+    const bonusPoints = 2.25 + (sex === 'f' ? 0.5 : 0) + 
+        ((sex === 'f' || !hasPartner) ? (numChildren || 0) : 0)
+
+    const perBonusPoint = 2580
+    const taxReduction = bonusPoints * perBonusPoint
+    const annualIncome = (income || 0) * 12
+
+    const taxStep = (v: number) => {
+        const steps = [[0, 0], [74640, .1], [107040, .14], [171840, .2], [238800, .31], [496920, .35], [640000, .47], [Infinity, .5]]
+        let total = 0
+        for (let i = 0; i < steps.length; ++i) {
+            const [value, rate] = steps[i]
+            if (v <= value) {
+                return total + (v - value) * rate
+            } else if (i) {
+                total += (value - steps[i - 1][0]) * rate
+            }
+        }
+
+        return Math.max(0, total)
+    }
+
+    const taxWithoutReductions = taxStep(annualIncome)
+    const annualPartnerIncome = partnerIncomeForCalculation * 12
+    const incomeTax = Math.max(0, taxWithoutReductions - taxReduction)
+    const partnerTax = Math.max(0, annualPartnerIncome ? taxStep(annualPartnerIncome) - 2.25 * perBonusPoint : 0)
+    const netAnnualPartnerIncome = annualPartnerIncome ? annualPartnerIncome - partnerTax : 0
+    const netIncome = annualIncome - incomeTax
+    const householdIncome = netIncome + netAnnualPartnerIncome
+    const netMonthlyHouseholdIncome = householdIncome / 12
+    const decile = (() => {
+        const deciles = [0, 4214, 6558, 8580, 11028, 13323, 16037, 19196, 23274, 29938, Infinity]
+        for (let i = 0; i < deciles.length; ++i) {
+            if (netMonthlyHouseholdIncome <= deciles[i]) {
+                return i
+            }
+        }
+    })()
+
+    const vatPerDecile = [0, 1190, 1208, 1355, 1606, 1731, 1885, 2014, 2189, 2389, 2920]
+
+    const monthlyVat = vatPerDecile[decile || 0]
+    const householdAnnualVat = monthlyVat * 12
+    const myAnnualVat = householdAnnualVat / (hasPartner ? 2 : 1)
+    const totalAnnualTax = myAnnualVat + Math.max(0, incomeTax)
+
+    return {
+        totalAnnualTax,
+        annualIncome,
+        netIncome,
+        householdIncome,
+        netMonthlyHouseholdIncome,
+        decile,
+        monthlyVat,
+        myAnnualVat,
+        taxWithoutReductions,
+        householdAnnualVat,
+        netAnnualPartnerIncome,
+        partnerTax,
+        incomeTax,
+        annualPartnerIncome,
+        bonusPoints,
+        taxReduction
+    }
+}
