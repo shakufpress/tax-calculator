51a50ff108fe549fd52c3f8a56ff6ed4cd5571d0
Zohar Guy <zohar.guy@gmail.com>
2020-02-24 09:59:40 +0200

    Cleanup

--- src/results/Results.tsx
+++ src/results/Results.tsx
@@ -59,57 +59,10 @@ const treeBeardData = useMemo(() => ({
     }), [incomeData, toTreeNode])
 
     treeBeardData.children = treeBeardData.children.filter(d => d.code !== "00");
-    // const history = useHistory()
-
-    // const onTabChange = useCallback(key => {
-    //     history.push(`/simple/results/${key}`)
-    // }, [history])
-
-    // const { resultKey } = useParams()
 
     return (<div>
-                    <TaxBarChart data={treeBeardData}/>
-                    </div>)
-
-//         <Tabs defaultActiveKey={resultKey} onChange={onTabChange} tabPosition="right">
-//             <TabPane tab="נתוני הכנסה" key="income">
-//             <h3>עיבוד נתונים</h3>
-//                 <Output label="הכנסה שנתית" value={shekel(tax.annualIncome)} />
-//                 <Output label="הכנסה נטו שלי (שנתי)" value={shekel(tax.netIncome)} />
-//                 <Output label="הכנסה שנתית של בן/בת הזוג" value={shekel(tax.annualPartnerIncome)} />
-//                 <Output label="מס הכנסה ללא נקודות זיכוי" value={shekel(tax.taxWithoutReductions)} />
-//                 <Output label="מספר נקודות זיכוי" value={tax.bonusPoints} />
-//                 <Output label="הפחת מס בזכות נקודות זיכוי" value={shekel(tax.taxReduction)} />
-//                 <Output label="סה״כ מס הכנסה שנתי" value={shekel(tax.incomeTax)} />
-//                 <Output label="מס שבן הזוג משלם" value={shekel(tax.partnerTax)} />
-//                 <Output label="הכנסה נטו של בן הזוג (שנתי)" value={shekel(tax.netAnnualPartnerIncome)} />
-//                 <Output label="הכנסה נטו למשק הבית" value={shekel(tax.householdIncome)} />
-//                 <Output label="חישוב הכנסה נטו חודשית" value={shekel(tax.netMonthlyHouseholdIncome)} />
-//                 <Output label="עשירון משק הבית" value={tax.decile || 0} />
-//                 <Output label="כמה מע״מ משק הבית משלם בחודש?" value={shekel(tax.monthlyVat)} />
-//                 <Output label="כמה מע״מ משק הבית משלם בשנה?" value={shekel(tax.householdAnnualVat)} />
-//                 <Output label="כמה מע״מ אני משלם בשנה?" value={shekel(tax.myAnnualVat)} />
-//                 <Output label="כמה מס אני משלם בשנה?" value={shekel(tax.totalAnnualTax)} />
-//             </TabPane>
-//             <TabPane tab="התפלגות מס" key="tax">
-//                 {incomeData.budget.total ? <div className="withBudget">
-//                     <Output label="פקטור" value={percent(tax.personalBudgetFactor)} />
-//                     <div className="budget">
-//                         <Treebeard data={treeBeardData} />
-//                     </div>
-//                 </div> : <span>Loading...</span>}
-//             </TabPane>
-//             <TabPane tab="תרשים התפלגות" key="pie">
-//                 <TaxPieChart data={treeBeardData.children}/>
-//             </TabPane>
-//             <TabPane tab="תרשים עמודות" key="bar">
-//                 <TaxBarChart data={treeBeardData}/>
-//             </TabPane>
-//             <TabPane tab="תרשימי התפלגות" key="charts">
-//                 <TaxPieChartsView data={treeBeardData}/>
-//             </TabPane>
-//   </Tabs>,
-//     </div>);
+                <TaxBarChart data={treeBeardData}/>
+            </div>);
 }
 
 export default Results;
