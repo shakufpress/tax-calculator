4ca4e513dacb15997f3aaf9413937d68b55f5b2b
Noam Rosenthal <noam@webkit.org>
2020-07-16 12:16:53 +0300

    Fix margins

--- src/results/Results.tsx
+++ src/results/Results.tsx
@@ -113,9 +113,10 @@ const Results = ({sex, tax, budget}: {sex: 'm' | 'f', tax: TaxData, budget: Budg
             </div>
         </div>
         {(tax.decile || 0) < 3 ? 
-        <div className="decile-disclaimer">
+                    <div className="content decile-content">
+                        <div className="decile-disclaimer">
             בעקבות ההון השחור, ייתכן שסכום המס שיוצג במחשבון יהיה גבוה מהקיים בפועל, במיוחד בעשירונים התחתונים.
-        </div> : null }
+        </div></div> : null }
         { budget ? <form className="search-form"><div className="content">
             <input className="search-bar" placeholder="חיפוש" type="text" onInput={e => setSearch((e.target as HTMLInputElement).value)} />
         </div></form> : null}
